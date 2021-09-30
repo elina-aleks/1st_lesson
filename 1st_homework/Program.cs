4c0427f65a990a2476a43819e79b9eccc699dac5
Elina <elina.aleksejeva@yahoo.com>
2021-09-30 21:11:14 +0300

    update

---
+++ 1st_homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1st_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Šis ir mans pirmais mājas darbs");
+        }
+    }
+}
